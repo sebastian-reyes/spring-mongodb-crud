597927379e117c3a849f3356154c92b73f48fd32
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-08-04 02:44:57 -0500

    Creando documento contacto

--- src/main/java/com/sreyes/app/api_rest_reactiva_mongodb/model/Contacto.java
+++ src/main/java/com/sreyes/app/api_rest_reactiva_mongodb/model/Contacto.java
@@ -1,4 +1,4 @@
-package com.sreyes.app.api_rest_reactiva_mongodb.documents;
+package com.sreyes.app.api_rest_reactiva_mongodb.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
