bcb21c4cc09774a3446316bbf798976e72b46595
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-08-04 01:58:43 -0500

    Iniciando repositorio

---
+++ src/main/java/com/sreyes/app/api_rest_reactiva_mongodb/ApiRestReactivaMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.sreyes.app.api_rest_reactiva_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestReactivaMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestReactivaMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreyes/app/api_rest_reactiva_mongodb/ApiRestReactivaMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyes.app.api_rest_reactiva_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestReactivaMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
